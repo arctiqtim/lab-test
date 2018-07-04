bac79a4e34524373a0376effb6699db60c981f4c
Tim Fairweather <tim.fairweather@arctiq.ca>
2018-07-04 11:40:11 -0400

    first commit

---
+++ org_mapping.py
@@ -0,0 +1,22 @@
+{
+ "Dept1": {
+  "users": [
+   "CN=Dept1_Users,CN=Users,DC=cloud,DC=lab"
+  ],
+  "remove_users": true,
+  "admins": [
+   "CN=Dept1_Admins,CN=Users,DC=cloud,DC=lab"
+  ],
+  "remove_admins": true
+ },
+ "Dept2": {
+  "users": [
+   "CN=Dept2_Users,CN=Users,DC=cloud,DC=lab"
+  ],
+  "remove_users": true,
+  "admins": [
+   "CN=Dept2_Admins,CN=Users,DC=cloud,DC=lab"
+  ],
+  "remove_admins": true
+ }
+}
